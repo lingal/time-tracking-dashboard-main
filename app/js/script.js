4fbdc43008224d0b371fd23addf8a0decfce7fe9
Linas Galumbauskas <galumbauskasl@gmail.com>
2022-01-21 09:55:37 +0000

    screenshots added to readme, breakpoint added for a smaller screen

--- app/js/script.js
+++ app/js/script.js
@@ -6,15 +6,15 @@ let html = '';
 
 window.addEventListener('DOMContentLoaded', function () {
   fetch('/data.json')
-  .then(function (response) {
-    return response.json();
-  }).then(function (jsonData) {
-    arr = jsonData;
-    arr.forEach(item => {
+    .then(function (response) {
+      return response.json();
+    }).then(function (jsonData) {
+      arr = jsonData;
+      arr.forEach(item => {
 
-      const bgClass = item.title.toLowerCase().split(' ')[0];
+        const bgClass = item.title.toLowerCase().split(' ')[0];
 
-      html += `<article class="activity-card activity-card__${bgClass} border-radius">
+        html += `<article class="activity-card activity-card__${bgClass} border-radius">
             <div class="card-content border-radius">
             
             <div class="card-content__top flex">
@@ -30,17 +30,17 @@ window.addEventListener('DOMContentLoaded', function () {
             
             <div class="card-content__bottom flex">
             <p>${item.timeframes["daily"].current}hrs</p>
-            <p><span>Last day -</span> ${item.timeframes["daily"].previous}hrs</p>
+            <p>Yesteray <span>-</span> ${item.timeframes["daily"].previous}hrs</p>
             
             </div >
             </div >
             </article > `
 
-    })
-    cardsEl.innerHTML = html;
+      })
+      cardsEl.innerHTML = html;
 
-  })
-  
+    })
+
 })
 
 
@@ -67,16 +67,13 @@ btns.forEach(btn => {
 
     switch (currentItem) {
       case 'daily':
-        timeframe = 'Day';
-
+        timeframe = 'Yesterday';
         break;
       case 'weekly':
-        timeframe = 'Week';
-
+        timeframe = 'Last Week';
         break;
       case 'monthly':
-        timeframe = 'Month';
-
+        timeframe = 'Last Month';
         break;
       default:
         timeframe = 'error';
@@ -100,7 +97,7 @@ btns.forEach(btn => {
               
               <div class="card-content__bottom flex">
               <p>${item.timeframes[currentItem].current}hrs</p>
-              <p><span>Last ${timeframe}</span> - ${item.timeframes[currentItem].previous}hrs</p>
+              <p>${timeframe}<span> - </span>${item.timeframes[currentItem].previous}hrs</p>
               
               </div >
               </div >
